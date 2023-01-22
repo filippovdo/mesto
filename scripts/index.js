bcfefc396c30330f486ff32f1f982b1d66985a9c
filippovdo <117165629+filippovdo@users.noreply.github.com>
2023-01-23 02:06:35 +0300

    финальные правки

--- scripts/index.js
+++ scripts/index.js
@@ -3,36 +3,31 @@ var buttonHide = document.querySelector('.popup__close');
 var popup = document.querySelector('.popup');
 
 let formElement = document.querySelector('.popup__content');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__text');
+let nameInput = document.querySelector('.popup__form_input_name');
+let jobInput = document.querySelector('.popup__form_input_about');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
 
+
 function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-
-    profileName.textContent = nameInput.value
-    profileText.textContent = jobInput.value
-
-  popup.classList.remove('popup_opened');
+  evt.preventDefault(); 
+  profileName.textContent = nameInput.value
+  profileText.textContent = jobInput.value
+  popupClose();
 }
 
-function Popup(evt){
-
+function popupOpen(){
   popup.classList.toggle('popup_opened');
-    nameInput.value = profileName.textContent
-    jobInput.value = profileText.textContent
+  nameInput.value = profileName.textContent
+  jobInput.value = profileText.textContent
+}
 
+function popupClose(){
+  popup.classList.toggle('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-buttonShow.addEventListener('click', Popup);
+buttonShow.addEventListener('click', popupOpen);
   
-buttonHide.addEventListener('click',  Popup);
-
-  
-  
-
-
-
+buttonHide.addEventListener('click',  popupClose);
