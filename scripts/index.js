924b4cbc23d478c996c5438cbe9e2f934860c337
filippovdo <117165629+filippovdo@users.noreply.github.com>
2023-02-13 02:56:25 +0300

    новый спринт 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 var buttonShow = document.querySelector('.profile__edit-button');
+
 var buttonHide = document.querySelector('.popup__close');
 var popup = document.querySelector('.popup');
 
@@ -31,3 +32,23 @@ formElement.addEventListener('submit', handleFormSubmit);
 buttonShow.addEventListener('click', popupOpen);
   
 buttonHide.addEventListener('click',  popupClose);
+
+
+var popup_two = document.querySelector('.popup__two');
+var cardButtonShow = document.querySelector('.profile__add-button');
+var cardButtonHide = document.querySelector('.popup__two .popup__close');
+
+function popupCardOpen(){
+  console.log('test')
+  popup_two.classList.add('popup_opened');
+//  nameInput.value = profileName.textContent
+//  jobInput.value = profileText.textContent
+}
+
+function popupCardClose(){
+  popup_two.classList.remove('popup_opened');
+}
+
+cardButtonShow.addEventListener('click', popupCardOpen);
+  
+cardButtonHide.addEventListener('click',  popupCardClose);
